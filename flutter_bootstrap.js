767ddc9cbcd3ebde1c39160bfb4b46fa4942f954
Adalberto Pompolo <beto.nassu@gmail.com>
2025-09-02 11:25:21 -0300

    chore: build #17

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"1e9a811bf8e70466596bcf0ea3a8b5adb5f17f
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3351989579"
+    serviceWorkerVersion: "2737346528"
   }
 });
